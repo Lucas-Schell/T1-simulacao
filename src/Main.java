cd53aefdca1f42f9a2d203dae01716d3f0968e74
Lucas Schell <lucas.schell@acad.pucrs.br>
2021-04-04 19:13:35 -0300

    random

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
+    private static final int a = 36789;
+    private static final int c = 14168;
+    private static final int m = 137921;
+    private static double x;
+    private static int randomCount = 0;
+
     public static void main(String[] args) {
-	// write your code here
+        double x = m / 2.0;
+    }
+
+    public static double nextRandom(int A, int B){
+        if(randomCount == 100000) return -1;
+        randomCount++;
+        x = (a * x + c) % m;
+        return (B - A) * (x / m) + A;
     }
 }
