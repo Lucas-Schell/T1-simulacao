cef0e895dfd33c61eb6920e5c3f5aaddb803b16d
Lucas Schell <lucas.schell@acad.pucrs.br>
2021-04-29 15:14:09 -0300

    input lista de configs

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,26 @@ public class Main {
     private static double x;
 
     public static void main(String[] args) {
-        int servers = 2;
-        int capacity = 3;
-        sim(servers, capacity, new int[]{2, 3}, new int[]{3, 5}, 2, 3, 1, new int[]{2, 5});
+        int[][] queues = new int[2][];
+        queues[0] = new int[]{2, 3, 2, 3, 2, 5}; //servers, capacity, arrivalMin, arrivalMax, exitMin, ExitMax
+        queues[1] = new int[]{1, 3, -1, -1, 3, 5};
+        sim(queues);
     }
 
-    public static void sim(int server, int capacity, int[] arrival, int[] exit, int queueCount, int capacity2, int server2, int[] move) {
-        int[] rowSize = new int[]{capacity + 1, capacity2 + 1}; //todo
-        int[] servers = new int[]{server, server2}; //todo
-        double[][] result = new double[queueCount][rowSize[0] + 2]; //todo
+    public static void sim(int[][] queues) {
+        int[] rowSize = new int[queues.length];
+        int[] servers = new int[queues.length];
+        for (int i = 0; i < queues.length; i++) {
+            rowSize[i] = queues[i][1] + 1;
+            servers[i] = queues[i][0];
+        }
+        double[][] result = new double[queues.length][rowSize[0] + 2];
         double[] seeds = {1345, 5423, 7863, 4423, 10587};
         for (int a = 0; a < 5; a++) {
             x = seeds[a];
-            double[][] timeCount = new double[queueCount][rowSize[0]]; //todo
-            int[] loss = new int[queueCount];
-            int[] queueSize = new int[queueCount];
+            double[][] timeCount = new double[queues.length][rowSize[0]];
+            int[] loss = new int[queues.length];
+            int[] queueSize = new int[queues.length];
             int randomCount = 0;
             double time = 0;
             LinkedList<Object[]> events = new LinkedList<>();
@@ -51,12 +56,13 @@ public class Main {
                         } else {
                             queueSize[arrivalQueue]++;
                             if (queueSize[arrivalQueue] <= servers[arrivalQueue]) {
-                                events.add(new Object[]{"M-" + arrivalQueue + "-" + (arrivalQueue + 1), nextRandom(move[0], move[1]) + time});
+                                events.add(new Object[]{"M-" + arrivalQueue + "-" + (arrivalQueue + 1),
+                                        nextRandom(queues[arrivalQueue][4], queues[arrivalQueue][5]) + time});
                                 randomCount++;
                             }
                         }
 
-                        events.add(new Object[]{"A-" + arrivalQueue, nextRandom(arrival[0], arrival[1]) + time});
+                        events.add(new Object[]{"A-" + arrivalQueue, nextRandom(queues[arrivalQueue][2], queues[arrivalQueue][3]) + time});
                         randomCount++;
 
                         events.remove(min);
@@ -70,7 +76,7 @@ public class Main {
                         time = (double) events.get(min)[1];
 
                         if (queueSize[exitQueue] >= servers[exitQueue]) {
-                            events.add(new Object[]{"E-" + exitQueue, nextRandom(exit[0], exit[1]) + time});
+                            events.add(new Object[]{"E-" + exitQueue, nextRandom(queues[exitQueue][4], queues[exitQueue][5]) + time});
                             randomCount++;
                         }
                         events.remove(min);
@@ -85,7 +91,7 @@ public class Main {
 
                         queueSize[out]--;
                         if (queueSize[out] >= servers[out]) {
-                            events.add(new Object[]{"M-" + out + "-" + in, nextRandom(move[0], move[1]) + time});
+                            events.add(new Object[]{"M-" + out + "-" + in, nextRandom(queues[out][4], queues[out][5]) + time});
                             randomCount++;
                         }
                         if (queueSize[in] >= rowSize[in] - 1) {
@@ -93,7 +99,7 @@ public class Main {
                         } else {
                             queueSize[in]++;
                             if (queueSize[in] <= servers[in]) {
-                                events.add(new Object[]{"E-" + in, nextRandom(exit[0], exit[1]) + time});
+                                events.add(new Object[]{"E-" + in, nextRandom(queues[in][4], queues[in][5]) + time});
                                 randomCount++;
                             }
                         }
